95ba6f55b60f8ed9eeae6f8f2a85343ed7493abd
Yeji Kim <125236337+yejimz@users.noreply.github.com>
2024-03-21 11:01:38 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "example" {
     ## 보안 취약점: 인터넷에 대한 액세스를 제어하지 않음
     # cidr_blocks = ["0.0.0.0/0"]
     # cidr_blocks = ["192.168.0.0/16"]
-    cidr_blocks = var.cidr_blocks
+    cidr_blocks = [var.cidr_blocks]
     description = "Allow all inbound traffic"
   }
 
@@ -21,7 +21,7 @@ resource "aws_security_group" "example" {
     protocol    = "-1"
     
     # cidr_blocks = ["0.0.0.0/0"]
-    cidr_blocks = var.cidr_blocks
+    cidr_blocks = [var.cidr_blocks]
     description = "Allow all outbound traffic"
   }
 }
